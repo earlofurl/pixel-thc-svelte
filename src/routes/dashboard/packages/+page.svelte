df759ef321576a8f15608fb54efb52e3abc277f1
earlofurl <jackthename@gmail.com>
2022-10-21 07:46:56 -0700

    added basic table
    Signed-off-by: earlofurl <jackthename@gmail.com>

--- src/routes/dashboard/packages/+page.svelte
+++ src/routes/dashboard/packages/+page.svelte
@@ -1,6 +1,9 @@
 <script>
+  import GeneralTable from "$lib/components/tables/GeneralTable.svelte";
+
   export let data;
 </script>
 
 <h1 class="font-bold text-2xl text-gray-900">Inventory</h1>
-{JSON.stringify(data)}
+<GeneralTable />
+<!--{JSON.stringify(data)}-->
